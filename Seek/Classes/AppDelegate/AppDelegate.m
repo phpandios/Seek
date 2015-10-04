39da1a15f0d45cb80f9709fccb1c5b17bf35d142
吴非凡 <wufeifan890330@126.com>
2015-10-04 22:37:54 +0800

    添加 友盟SDK

--- Seek/Classes/AppDelegate/AppDelegate.m
+++ Seek/Classes/AppDelegate/AppDelegate.m
@@ -7,7 +7,10 @@
 //
 
 #import "AppDelegate.h"
-
+#import "UMSocial.h"
+#import "UMSocialData.h"
+#import "UMSocialQQHandler.h"
+#import "UMSocialSnsService.h"
 @interface AppDelegate ()
 
 @end
@@ -17,7 +20,81 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [UMSocialData setAppKey:kUMAppKey];
+    [UMSocialData openLog:YES];
+    
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = ({
+        UIViewController *vc = [UIViewController new];
+        vc.view.backgroundColor = [UIColor cyanColor];
+        vc;
+    });
+    
+    [self.window makeKeyAndVisible];
+    
+    // QQ登陆 NO
+    [UMSocialQQHandler setQQWithAppId:kUMQQAppID appKey:kUMQQAppKey url:kUMUrl];
+    
+    UMSocialSnsPlatform *snsPlatform = [UMSocialSnsPlatformManager getSocialPlatformWithName:UMShareToQQ];
+    
+    snsPlatform.loginClickHandler(self.window.rootViewController,[UMSocialControllerService defaultControllerService],YES,^(UMSocialResponseEntity *response){
+        
+        //          获取微博用户名、uid、token等
+        
+        if (response.responseCode == UMSResponseCodeSuccess) {
+            
+            NSLog(@"%@", [UMSocialAccountManager socialAccountDictionary]);
+            NSLog(@"%d", [UMSocialAccountManager isLogin]);
+            
+            UMSocialAccountEntity *snsAccount = [[UMSocialAccountManager socialAccountDictionary] valueForKey:UMShareToQQ];
+            
+            NSLog(@"username is %@, uid is %@, token is %@ url is %@",snsAccount.userName,snsAccount.usid,snsAccount.accessToken,snsAccount.iconURL);
+            
+           
+            
+        }});
+    
+    //获取accestoken以及QQ用户信息，得到的数据在回调Block对象形参respone的data属性
+    [[UMSocialDataService defaultDataService] requestSnsInformation:UMShareToQQ  completion:^(UMSocialResponseEntity *response){
+        NSLog(@"SnsInformation is %@",response.data);
+        UMSocialAccountEntity *snsAccount = [[UMSocialAccountManager socialAccountDictionary] valueForKey:UMShareToQQ];
+        
+        NSLog(@"username is %@, uid is %@, token is %@ url is %@",snsAccount.userName,snsAccount.usid,snsAccount.accessToken,snsAccount.iconURL);
+    }];
+    
+    // 分享 OK
+//    [UMSocialSnsService presentSnsIconSheetView:self.window.rootViewController
+//                                         appKey:kUMAppKey
+//                                      shareText:@"友盟社会化分享让您快速实现分享等社会化功能，http://umeng.com/social"
+//                                     shareImage:[UIImage imageNamed:@"icon.png"]
+//                                shareToSnsNames:@[UMShareToTencent]
+//                                       delegate:nil];
     return YES;
+}
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    return  [UMSocialSnsService handleOpenURL:url];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+
+{
+    
+    // 登录需要编写
+    
+    [UMSocialSnsService applicationDidBecomeActive];
+    
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    return  [UMSocialSnsService handleOpenURL:url];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -34,9 +111,6 @@
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
