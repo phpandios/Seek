56cffa70e5528ae815547c54f77374d6b649c4f7
吴非凡 <wufeifan890330@126.com>
2015-10-04 14:57:00 +0800

    Init Version

---
+++ Seek/Classes/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Seek
+//
+//  Created by 吴非凡 on 15/10/4.
+//  Copyright © 2015年 吴非凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Seek/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Seek
+//
+//  Created by 吴非凡 on 15/10/4.
+//  Copyright © 2015年 吴非凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
